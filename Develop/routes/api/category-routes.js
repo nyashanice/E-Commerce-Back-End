9c33c4b4e3984dce5c4cb59448800bdb02c404cb
Nya Tyus <nyastyus@gmail.com>
2023-03-06 18:49:20 -0500

    create category update route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -45,8 +45,27 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    if (!updateCategory) {
+      res.status(404).json({ message: "No category found with that ID" });
+      return;
+    }
+    res.status(200).json(updateCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete("/:id", async (req, res) => {
